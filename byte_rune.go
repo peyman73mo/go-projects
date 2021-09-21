b0d371260c40c78c1cf8a18cefe11618bdf330e9
peyman73mo <peyman.m1373@gmail.com>
2021-09-21 15:40:48 -0400

    this is for byte and rune variable type(character)

---
+++ byte_rune.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"reflect" // for printing type of variable
+)
+
+func main() {
+	var chr1 = 'A' //rune type (by default)
+	// byte type :
+	chr2 := byte('a')
+	// chr2 := []byte("a")
+
+	fmt.Printf("type: %T value: %v\n", chr1, chr1)
+	fmt.Println("type: ", reflect.TypeOf(chr2), "value: ", chr2)
+	if chr2 == 97 {
+		fmt.Println("yes")
+	} else {
+		fmt.Println("no")
+	}
+
+}
