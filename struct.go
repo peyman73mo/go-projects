b94477d3f23192206f6d60414a48b6744a775121
peyman73mo <peyman.m1373@gmail.com>
2021-09-21 16:19:05 -0400

    using method

--- struct.go
+++ struct.go
@@ -4,32 +4,40 @@ import (
 	"fmt"
 )
 
-type t1 struct {
+type T1 struct {
 	name string
 	id   int
 }
 
+// method syntax :
+// func (receiver) func_name (parameters) return_type
+func (t1 T1) show() {
+	fmt.Println("name :", t1.name)
+	fmt.Println("id :", t1.id)
+}
+
 func main() {
 
-	var a1 t1 = t1{
+	var a1 T1 = T1{
 		name: "peyman",
 		id:   1,
 	}
-	var a2 = t1{
+	a2 := T1{
 		"jack",
 		2,
 	}
 
 	f2(&a1)
+	fmt.Println(a1)
+
 	f1(a2)
 	a2.id++
-	fmt.Println(a1)
-	fmt.Println(a2)
+	a2.show()
 }
 
-func f1(u t1) {
+func f1(u T1) {
 	u.id = 0
 }
-func f2(u *t1) {
+func f2(u *T1) {
 	u.id = 0
 }
