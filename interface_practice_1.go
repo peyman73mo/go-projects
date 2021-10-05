72b40d8b7fcc576658b30121656c6fb92471e7c4
peyman73mo <peyman.m1373@gmail.com>
2021-10-04 20:41:43 -0400

    Custom Type Declaration

--- interface_practice_1.go
+++ interface_practice_1.go
@@ -14,11 +14,22 @@ func (s S) F1() {
 	fmt.Println(s.info)
 }
 
+type T string
+
+func (t T) print() {
+	fmt.Println(t)
+}
+
 func main() {
-	var a F = S{"this is S type struct"}
-	fmt.Printf("type : %T\n", a)
-	// *** this is a error  ***
-	// fmt.Println(a.info)
-	a.F1()
+	// var a F = S{"this is S type struct"}
+
+	// fmt.Printf("type : %T\n", a)
+	// // *** this is a error  ***
+	// // fmt.Println(a.info)
+	// a.F1()
+	var a T = "Nothing"
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", T("alaki"))
+	a.print()
 
 }
