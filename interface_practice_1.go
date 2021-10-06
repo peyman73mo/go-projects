57e57b4d0914d40d8dc9c630fcc530821d53709c
peyman73mo <peyman.m1373@gmail.com>
2021-10-05 23:27:01 -0400

    Custom Type Declaration

--- interface_practice_1.go
+++ interface_practice_1.go
@@ -1,35 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+// ----------------------------------------------------------------
 type F interface {
 	F1()
+	foo()
 }
 
+// ----------------------------------------------------------------
+// S has to have both foo() and F1 for being F interface
 type S struct {
 	info string
 }
 
+func (s S) foo() {
+}
 func (s S) F1() {
 	fmt.Println(s.info)
 }
 
+// ----------------------------------------------------------------
 type T string
 
 func (t T) print() {
 	fmt.Println(t)
 }
 
+// ----------------------------------------------------------------
+// type T1 = strings
+
 func main() {
 	// var a F = S{"this is S type struct"}
-
 	// fmt.Printf("type : %T\n", a)
+	// ----------------------------------------------------------------
 	// // *** this is a error  ***
 	// // fmt.Println(a.info)
+
 	// a.F1()
-	var a T = "Nothing"
+	// ----------------------------------------------------------------
+	a := T("Nothing")
 	fmt.Printf("%T\n", a)
-	fmt.Printf("%T\n", T("alaki"))
 	a.print()
+	// ----------------------------------------------------------------
+	// fmt.Printf("%T\n", T("alaki"))
 
 }
